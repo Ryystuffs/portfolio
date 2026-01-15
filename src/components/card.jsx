089d7fa95cfb623c02f4fb7827ec216788e593d9
Ryystuffs <131592912+Ryystuffs@users.noreply.github.com>
2026-01-15 14:48:19 +0800

    Fix: design and layout of the project cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,25 +1,39 @@
-import iconGithub from '../assets/github.png'
-export const Card = ({pic, title, description, techStack, link}) => {
+import iconGithub from '../assets/github-logo.png'
+export const Card = ({pic, title, role, description, techStack, link}) => {
     return (
-        <div className="bg-[#1f2839] lg:flex sm:grid-rows-2 line line-clamp-4 xjustify-around items-center rounded-lg p-5 my-3 hover:-translate-y-2 transition-all ease-in-out duration-700">
-            <div className="flex justify-center min-w-[50%] h-80 object-cover">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5 rounded-lg">
+            <div className="py-7 px-13">
                 <img src={pic} alt={title} className="rounded-lg"/>
             </div>
-            <div className="flex-col items-center justify-center max-w-[50%] mx-5">
-                <p className='font-bold my-5 text-2xl'>{title}</p>
-                <p>{description}</p>
-                <div className="flex flex-wrap justify-center gap-2 my-7">
+            <div className="">
+                <p className='font-bold mt-5 text-3xl'>{title}</p>
+                <p className='mt-5 text-xl'>{role}</p>
+                <hr className="my-4"/>
+                <p className='leading-8'>{description}</p>
+                <div className="my-5">
+                    <p className="font-bold mb-7 text-xl">Technologies:</p>
+                    <div className="flex flex-wrap gap-3">
                     {techStack.map((tech,index)=>(
                         <span
                             key={index}
-                            className="bg-purple-900 text-blue-200 px-3 py-1 rounded-full"
+                            className="px-7 py-2 bg-gray-800 text-purple-400 rounded-full text-sm font-medium inline-block"
                         >
                             {tech}  
                         </span>
                     ))}
+                    </div>
                 </div>
-                <div className="my-4 bg-pink-400 p-3 rounded-lg w-50 flex items-center hover:-translate-y-2 transition-all ease-in-out duration-700">
-                    <a href={link} target='_blank' className='flex gap-2 items-center text-gray-800'><img src={iconGithub} alt="" className='h-10 w-10'/>View on Github</a>
+                <div className="flex">
+                    <a
+                        href={link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mt-5 w-48 border border-white rounded-3xl py-3 px-5 
+                                flex items-center gap-3 hover:bg-white hover:text-black transition"
+                    >
+                        <img src={iconGithub} alt="GitHub icon" className="h-6 w-6" />
+                        <span className="text-sm font-medium">View on GitHub</span>
+                    </a>
                 </div>
             </div>
         </div>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -7,6 +7,7 @@ export const Projects = () => {
         {
             pic: project1,
             title: 'Gym Membership Management System',
+            role: 'Full-Stack Lead Developer',
             description:'This Gym Management System is a full-stack web application designed to automate facility operations and member attendance. Built on the Laravel framework, the system features a secure, real-time check-in architecture utilizing Simple QrCode and Html5-QrCode for seamless generation and scanning. The user interface is crafted with Tailwind CSS for a modern, responsive experience, while the backend is powered by MySQL and XAMPP Apache to ensure robust data management and reliable performance.',
             techStack:['Laravel','Javascript','Blade','MySQL','SweetAlert'],
             link: 'https://github.com/Ryystuffs/gymsystem'
@@ -14,6 +15,7 @@ export const Projects = () => {
         {
             pic: project2,
             title: 'Travel Guide',
+            role: 'Full-Stack Lead Developer',
             description:'A simple travel guide website that helps people plan their trips easily. It provides clear information on the best places to visit, helpful travel tips, and ready-made schedules. The design is easy to use on any device, allowing travelers to find locations and organize their trip details in one place.',
             techStack:['PHP','Javascript','MySQL'],
             link: 'https://github.com/Ryystuffs/BAO-RESERVATION'
@@ -21,22 +23,24 @@ export const Projects = () => {
         {
             pic: project3,
             title: 'Reservation Management System',
+            role: 'Full-Stack Lead Developer',
             description:'A streamlined reservation system built to simplify school transactions. It helps students easily purchase books, uniforms, and merchandise, reducing long wait times and making the process at the Business Affairs Office much faster and more organized.',
             techStack:['C++','Visual Studio','MySQL'],
             link: 'https://github.com/Ryystuffs/compass_php_project'
         }   
     ];
     return (
-        <section id="projects" className="flex flex-col justify-center items-center mt-20 md:mt-45 mb-35 ">
-            <div className="text-center text-purple-800 text-[52px] font-extrabold mb-10">Projects</div>
-            <div className="flex flex-wrap justify-center gap-2 max-w-[80%]">
+        <section id="projects" className="">
+            <div className="text-[52px] font-extrabold mt-5 mx-10 px-10">Projects</div>
+            <div className="p-10">
                 {projects.map((proj,index)=>(
                     <Card
                         key={index}
                         pic={proj.pic}
                         title={proj.title}
+                        role={proj.role}
                         description={proj.description}
-                        techStack={proj.techStack}
+                        techStack={proj.techStack}  
                         link={proj.link}
                     />
                 ))}
